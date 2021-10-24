cf059522ddf062d2c7d757b5b299311da89df0c7
Alvaro Cabero <alvarocabero@gmail.com>
2021-10-24 23:57:21 +0200

    :memo: Added pagination funcitonality from NavBar

--- src/components/NavBar/NavBar.js
+++ src/components/NavBar/NavBar.js
@@ -1,11 +1,18 @@
-const NavBar = () => {
+const NavBar = ({ page, nextPage, prevPage }) => {
   return (
     <nav className="nav-bar">
-      <button className="nav-bar__button1 btn btn-primary btn-lg btn-info">
-        Patrás
+      <p className="nav-bar__text">{page}</p>
+      <button
+        className="nav-bar__button1 btn btn-outline-primary"
+        onClick={prevPage}
+      >
+        Prev
       </button>
-      <button className="nav-bar__button2 btn btn-primary btn-lg btn-info">
-        Palante
+      <button
+        className="nav-bar__button2 btn btn-outline-primary"
+        onClick={nextPage}
+      >
+        Next
       </button>
     </nav>
   );
